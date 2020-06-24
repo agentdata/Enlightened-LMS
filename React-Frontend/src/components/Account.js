83de0507b7cb249c4fd9f695ea6ff0ec6b8243fa
Constance Timmerman <ctimmerman@mail.weber.edu>
2020-06-24 14:52:14 -0600

    Added new component for account

---
+++ React-Frontend/src/components/Account.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class Account extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            ""
+        );
+    }
+}
