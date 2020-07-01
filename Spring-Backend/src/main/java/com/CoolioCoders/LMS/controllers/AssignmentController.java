ca8aa39cb43abee527d7a13e028ea0614ffdfffb
JPD333 <jordanpdavis@hotmail.com>
2020-07-01 00:38:25 -0600

    Added an endpoint to get list of assignments based on course. Must call using enrolled student or course instructor token.

--- Spring-Backend/src/main/java/com/CoolioCoders/LMS/controllers/AssignmentController.java
+++ Spring-Backend/src/main/java/com/CoolioCoders/LMS/controllers/AssignmentController.java
@@ -1,25 +1,20 @@
 package com.CoolioCoders.LMS.controllers;
 
-import com.CoolioCoders.LMS.models.*;
+import com.CoolioCoders.LMS.models.Assignment;
+import com.CoolioCoders.LMS.models.Course;
+import com.CoolioCoders.LMS.models.SubmissionType;
+import com.CoolioCoders.LMS.models.User;
 import com.CoolioCoders.LMS.services.AssignmentService;
 import com.CoolioCoders.LMS.services.CourseService;
 import com.CoolioCoders.LMS.services.LMSUserDetailsService;
-import jdk.internal.loader.AbstractClassLoaderValue;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
-import java.text.ParseException;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -75,14 +70,41 @@ public class AssignmentController {
                 model.put("message", "Error: invalid instructor/course combination");
             }
 
-            return ok(model);
-
         } catch (Exception e) {
             e.printStackTrace();
             model.put("message", e.getMessage());
-            return ok(model);
         }
+        return ok(model);
     }
 
+
+    @PreAuthorize("hasAnyAuthority({'INSTRUCTOR', 'STUDENT'})")
+    @GetMapping("/{courseId}")
+    public ResponseEntity<Map<Object, Object>> userCourseAssignments(Principal principalUser, @PathVariable String courseId){
+        Map<Object, Object> model = new HashMap<>();
+        try {
+            User user = userService.findUserByEmail(principalUser.getName());
+            Course course = courseService.findById(courseId);
+
+            // user making request must either be a student enrolled in course or the course instructor
+            if(user != null && course != null && (course.getInstructor().getId().equals(user.getId()) ||
+                                                  courseService.findStudentsInCourse(course).contains(user))){
+
+                model.put("assignments", assignmentService.findByCourseId(courseId));
+
+                //if no error then return success message and OK status
+                model.put("message", "Success");
+            }
+            else {
+                model.put("message", "Error: invalid user/course combination");
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            model.put("message", e.getMessage());
+        }
+        return ok(model);
+
+    }
 
 }
--- Spring-Backend/src/main/java/com/CoolioCoders/LMS/services/AssignmentService.java
+++ Spring-Backend/src/main/java/com/CoolioCoders/LMS/services/AssignmentService.java
@@ -5,11 +5,22 @@ import com.CoolioCoders.LMS.repositories.AssignmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class AssignmentService {
 
     @Autowired
     AssignmentRepository assignmentRepository;
+
+    public List<Assignment> findByCourseId(String courseId){
+        List<Assignment> assignments = assignmentRepository.findByCourseId(courseId);
+        if(assignments != null){
+            return assignments;
+        }
+        return new ArrayList<>();
+    }
 
     public Assignment save(Assignment assignment){
         return assignmentRepository.save(assignment);
