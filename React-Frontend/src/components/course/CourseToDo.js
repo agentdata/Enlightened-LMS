8a25d58c228ee640763dc8f58810baa95277196a
Constance Timmerman <ctimmerman@mail.weber.edu>
2020-07-13 13:11:03 -0600

    Modified http.js to include get for assignments, modified CourseToDo to utilize API call to get assignments

--- React-Frontend/src/components/course/CourseToDo.js
+++ React-Frontend/src/components/course/CourseToDo.js
@@ -1,37 +1,38 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link'
+import http from '../../api/http';
 
-const state = {
-  isLoggedIn: false,
-  assignments: [
-      {
-          title: 'Assignment 3',
-          course: 'CS 3100',
-          due: '2020-06-13T23:59:59Z',
-          link: 'http://www.google.com'
-      },
-      {
-          title: 'Quiz 3',
-          course: 'CS 3260',
-          due: '2020-06-14T23:59:59Z',
-          link: 'link to assignment'
-      },
-      {
-          title: 'Assignment 4',
-          course: 'CS 3260',
-          due: '2020-06-16T23:59:59Z',
-          link: 'link to assignment'
-      }
-  ]
-};
+// const state = {
+//   isLoggedIn: false,
+//   assignments: [
+//       {
+//           title: 'Assignment 3',
+//           course: 'CS 3100',
+//           due: '2020-06-13T23:59:59Z',
+//           link: 'http://www.google.com'
+//       },
+//       {
+//           title: 'Quiz 3',
+//           course: 'CS 3260',
+//           due: '2020-06-14T23:59:59Z',
+//           link: 'link to assignment'
+//       },
+//       {
+//           title: 'Assignment 4',
+//           course: 'CS 3260',
+//           due: '2020-06-16T23:59:59Z',
+//           link: 'link to assignment'
+//       }
+//   ]
+// };
 
-const useStyles = makeStyles((theme) => ({
+const styles = theme => ({
   root: {
     minWidth: '220px',
     backgroundColor: theme.palette.background.paper,
@@ -41,46 +42,81 @@ const useStyles = makeStyles((theme) => ({
   inline: {
     display: 'inline',
   },
-}));
+});
 
-export default function HomeToDo() {
-  const classes = useStyles();
+class HomeToDo extends React.Component {
 
-  return (
-    <List className={classes.root}>
+  constructor(props) {
+    super(props);
 
-      <ListItem alignItems="flex-start">
-          <ListItemText primary="Upcoming Assignments" />
-      </ListItem>
-      <Divider component="li" />
+    this.state = {
+        isLoggedIn: false,
+        assignments: []
+      };
+  }
 
-      {state.assignments ? ( 
-        state.assignments.map(currentAssignment => (
-          <ListItem alignItems="flex-start" key={currentAssignment.title + " " + currentAssignment.due}>
-            <Link href={currentAssignment.link}>
-              <ListItemText
-                primary={currentAssignment.title}
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      component="span"
-                      variant="body2"
-                      className={classes.inline}
-                      color="textPrimary">
-                        {currentAssignment.course}
-                      </Typography>
-                      <br />
-                      {"Due: " + currentAssignment.due}
-                  </React.Fragment>
-                }
-                />
-              <Divider />
-            </Link>
-          </ListItem>
-        ))
-        
-      ) : <div>No upcoming assignments</div>
+  getUpcomingAssignments() {
+    http.getUpcomingAssignments()
+    .then( async (response) => {
+      const body = await response.json();
+      if (response.status === 200 && body["message"] === "success") {
+        console.log(body)
+        var assignments = [];
+        for (let a in body["assignments"]) {
+          this.setState({
+            assignments: [...this.state.assignments, a]
+          })
+        }
+      }
+    })
+  }
+
+  componentDidMount() {
+    this.getUpcomingAssignments();
+    console.log(this.state.assignments);
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <List className={classes.root}>
+  
+        <ListItem alignItems="flex-start">
+            <ListItemText primary="Upcoming Assignments" />
+        </ListItem>
+        <Divider component="li" />
+  
+        {this.state.assignments ? ( 
+          this.state.assignments.map(currentAssignment => (
+            <ListItem alignItems="flex-start" key={currentAssignment.title + " " + currentAssignment.due}>
+              <Link href={currentAssignment.link}>
+                <ListItemText
+                  primary={currentAssignment.title}
+                  secondary={
+                    <React.Fragment>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        className={classes.inline}
+                        color="textPrimary">
+                          {currentAssignment.course}
+                        </Typography>
+                        <br />
+                        {"Due: " + currentAssignment.due}
+                    </React.Fragment>
+                  }
+                  />
+                <Divider />
+              </Link>
+            </ListItem>
+          ))
+          
+        ) : <div>No upcoming assignments</div>
+      }
+      </List>
+    );
     }
-    </List>
-  );
 }
+
+export default withStyles(styles)(HomeToDo);
