d5ce6e864e28d84baeeb0961e39e002c6fd20810
JFujitaWSU <juliandean@mail.weber.edu>
2020-07-23 15:44:44 -0600

    Endpoint for decrementing balance

---
+++ Spring-Backend/src/main/java/com/CoolioCoders/LMS/models/Payment.java
@@ -0,0 +1,9 @@
+package com.CoolioCoders.LMS.models;
+
+public class Payment {
+
+    float amount;
+
+    public void setAmount(float amount){this.amount = amount;}
+    public float getAmount(){return amount;}
+}
