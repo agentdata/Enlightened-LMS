bbbaf4863b56aa4ea704e0bd7c636a90be6f2ad8
Constance Timmerman <ctimmerman@mail.weber.edu>
2020-07-27 16:45:25 -0600

    Added component subdirectory for analytics charts, created skeleton for StudentGradesChart component

---
+++ React-Frontend/src/components/charts/StudentGradesChart.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ResponsiveBar } from '@nivo/bar'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = theme => ({
+    //styles
+})
+
+class StudentGradesChart extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(StudentGradesChart)
+
