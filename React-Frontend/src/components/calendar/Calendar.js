e4ad80b0e46c9867d0252cb05f7bfdee2eaf5473
Constance Timmerman <ctimmerman@mail.weber.edu>
2020-07-29 09:23:26 -0600

    updated package.json with new packages, adding functionalities to Calendar.js

--- React-Frontend/src/components/calendar/Calendar.js
+++ React-Frontend/src/components/calendar/Calendar.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Container, Paper, Table, TableContainer, TableHead, TableBody,
-     TableRow, TableCell, TablePagination, Toolbar, Typography } from '@material-ui/core';
+import { Container, Modal } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
+import CourseAssignment from '../course/CourseAssignment';
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from 'moment';
 
@@ -10,6 +10,9 @@ const localizer = momentLocalizer(moment);
 
 const styles = withStyles => ({
     calendar: {
+        
+    },
+    modal: {
         
     }
   });
@@ -20,16 +23,50 @@ class LMSCalendar extends React.Component {
         super(props)
 
         this.state = {
+            selectedAssignment: null,
             events: [
                 {
                     start: moment().toDate(),
                     end: moment()
-                        .add(1, "days")
                         .toDate(),
-                    title: "Some event title"
+                    title: "Some event title",
                 }
             ]
         };
+    }
+
+    loadEvents() {
+        // API call goes here -- need to load in the due assignment
+
+        // Inside if (response.status === 200) block:
+        
+        // this.setState({
+        //     events: [...events, dueAssignment]
+        // })
+    }
+
+    componentDidMount() {
+        this.loadEvents();
+    }
+
+    componentDidUpdate() {
+        
+    }
+
+    handleAssignmentClose = () => {
+    }
+
+    setAssignmentClicked() {
+    }
+
+    handleSelectEvent = () => {
+        console.log("stuff")
+        this.setState({
+            selectedAssignment: {
+                title: "Assignment",
+                description: "Desc"
+            }
+        })
     }
 
     render() {
@@ -43,7 +80,22 @@ class LMSCalendar extends React.Component {
                     defaultView="month"
                     events={this.state.events}
                     style={{ height: "90vh" }}
+                    onSelectEvent={this.handleSelectEvent}
                     />
+                    {/* {this.selectedAssignment && (
+                        <Modal
+                        className={classes.modal}
+                        disableBackdropClick
+                        open={assignmentModalOpen}
+                        onClose={handleAssignmentClose}
+                        aria-labelledby="simple-modal-title"
+                        aria-describedby="simple-modal-description"
+                        >
+                            <div className={classes.paper}>
+                                <CourseAssignment closeModal = {this.handleAssignmentClose()} assignmentClicked = {this.selectedAssignment}/>
+                            </div>
+                        </Modal>
+                    )} */}
             </Container>
         )
     }
