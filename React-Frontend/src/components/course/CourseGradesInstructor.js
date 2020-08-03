d1bdd43693fa6ce8c30da66be42690943311045a
Constance Timmerman <ctimmerman@mail.weber.edu>
2020-08-03 09:39:15 -0600

    Working on CourseGradesInstructor table

--- React-Frontend/src/components/course/CourseGradesInstructor.js
+++ React-Frontend/src/components/course/CourseGradesInstructor.js
@@ -1,23 +1,138 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import { withStyles } from "@material-ui/core/styles"
-import { Button } from "@material-ui/core"
+import { Box, Button, Collapse, Table, TableBody, TableCell, TableContainer, TableHead, 
+    TablePagination, TableRow, TableSortLabel, Typography, Checkbox, Paper } from '@material-ui/core';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import IconButton from '@material-ui/core/IconButton';
+import utilities from '../../actions/utilities';
 import http from '../../api/http';
+import TeacherGradesChart from '../charts/StudentGradesChart';
 
 const styles = theme => ({
-
+    root: {
+        flexGrow: 1,
+      },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
 })
 
-class CourseGradesInstructor extends Component {
+function Row(props) {
+    const { assignment } = props;
+    const [open, setOpen] = React.useState(false);
 
-    constructor(props) {
-        super(props);
+    return (
+        <React.Fragment>
+            <TableRow key={assignment.title}>
+                <TableCell>
+                    <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    {assignment.title}
+                </TableCell>
+                <TableCell align="right">{utilities.formatDateTime(assignment.dueDate)}</TableCell>
+                <TableCell align="right">{assignment.submissions.length === 0? "No submissions": assignment.submissions.length}</TableCell>
+                <TableCell align="right">{assignment.graded === undefined ? 0 : assignment.graded}</TableCell>
+                <TableCell align="right">{assignment.maxPoints}</TableCell>
+                <TableCell align="right">{/*assignment.averageScore*/} avg</TableCell>
+                <TableCell align="right">Stuff</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box style={{height: '550px', width: '600px'}}>
+                            <Typography variant="h6" gutterBottom component="div">
+                                Grade Analytics
+                            </Typography>
+                            <TeacherGradesChart assignment={assignment}></TeacherGradesChart>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    );
+}
 
-        
+export default function CourseGradesInstructor() {
 
-        this.state = {
-            courseId: sessionStorage.getItem('courseId'),
-            assignments: [
+    const classes = styles;
+    const [assignments, setAssignments] = React.useState([]);
+    const [assignmentClicked, setAssignmentClicked] = React.useState({assignmentId: -1});
+    const courseId = sessionStorage.getItem('courseId');
+
+
+    // API call
+    useEffect(() => {
+        var fetchedAssignments = []
+        http.getCourseAssignmentsWithDetails(sessionStorage.getItem("courseId"))
+        .then( async(response) => {
+            var body = await response.json();
+            if(response.status === 200 && body["message"] === "Success"){
+            for (let i in body["assignments"]) {
+                let assignment = {
+                title: body["assignments"][i]["title"],
+                description: body["assignments"][i]["description"],
+                maxPoints: body["assignments"][i]["maxPoints"],
+                dueDate: body["assignments"][i]["dueDate"],
+                assignmentID: body["assignments"][i]["id"],
+                submissionType: body["assignments"][i]["submissionType"],
+                submitted: false,
+                }
+                if(body["assignments"][i]["submissions"] !== null){
+                    assignment.submitted = true
+                    assignment.graded = body["assignments"][i]["submissions"]["graded"]
+                if(body["assignments"][i]["submissions"]["pointsAwarded"] !== null){
+                    assignment.pointsAwarded = body["assignments"][i]["submissions"]["pointsAwarded"]
+                }
+                assignment.submissions = body["assignments"][i]["submissions"]
+                
+                }
+                fetchedAssignments.push(assignment)
+            }  
+            // set state
+            setAssignments(fetchedAssignments)
+            }
+        })
+        .catch((e) => {
+            console.warn("There was an error retrieving instructor courses: ", e);
+        });
+    }, [assignmentClicked]);
+
+    return (
+        <div>
+            <TableContainer component={Paper}>
+                <Typography variant="h4">Grades</Typography>
+                <Table className={classes.table} aria-label="grades table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell></TableCell>
+                            <TableCell>Title</TableCell>
+                            <TableCell align="right">Due</TableCell>
+                            <TableCell align="right">Submissions</TableCell>
+                            <TableCell align="right">Graded</TableCell>
+                            <TableCell align="right">Out of</TableCell>
+                            <TableCell align="right">Class average</TableCell>
+                            <TableCell align="right">Graded Submissions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {assignments.map((assignment) => (
+                        <Row key={ assignment.title } assignment= { assignment } />
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    )
+        // this.state = {
+        //     courseId: sessionStorage.getItem('courseId'),
+        //     assignments: [
                 // {
                 //     title: "Assignment 1",
                 //     assignmentId: 12345,
@@ -45,83 +160,6 @@ class CourseGradesInstructor extends Component {
                 //     maxPoints: 150,
                 //     average: 130
                 // }
-            ]
-        }
-        
-    }
-
-    componentDidMount() {
-        var fetchedAssignments = []
-        http.getCourseAssignmentsWithDetails(sessionStorage.getItem("courseId"))
-        .then( async(response) => {
-            var body = await response.json();
-            if(response.status === 200 && body["message"] === "Success"){
-            for (let i in body["assignments"]) {
-                let assignment = {
-                title: body["assignments"][i]["title"],
-                description: body["assignments"][i]["description"],
-                maxPoints: body["assignments"][i]["maxPoints"],
-                dueDate: body["assignments"][i]["dueDate"],
-                assignmentID: body["assignments"][i]["id"],
-                submissionType: body["assignments"][i]["submissionType"],
-                submitted: false,
-                }
-                if(body["assignments"][i]["submissions"] !== null){
-                    assignment.submitted = true
-                    assignment.graded = body["assignments"][i]["submissions"]["graded"]
-                if(body["assignments"][i]["submissions"]["pointsAwarded"] !== null){
-                    assignment.pointsAwarded = body["assignments"][i]["submissions"]["pointsAwarded"]
-                }
-                assignment.Submission = body["assignments"][i]["submissions"]
-                
-                }
-                
-                fetchedAssignments.push(assignment)
-            }  
-            // set state
-            this.setState({assignments: fetchedAssignments})
-            }
-        })
-        .catch((e) => {
-            console.warn("There was an error retrieving instructor courses: ", e);
-        });
-    }
-  
-      
-
-    render() {
-        const classes = this.props
-        return (
-            <div className={classes.studentGradeView}>
-                <table border = "5px" cellPadding="7px">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Due</th>
-                            <th>Submissions</th>
-                            <th>Graded</th>
-                            <th>Out Of</th>
-                            <th>Class Average</th>
-                            <th>Grade Submissions</th>
-                        </tr>
-                    </thead>
-                <tbody>
-                {this.state.assignments.map(assignment =>(
-                    <tr key={assignment.title}>
-                        <td>{assignment.title}</td>
-                        <td>{assignment.dueDate}</td>
-                        <td>{assignment.Submission.length}</td>
-                        <td>{assignment.graded === undefined ? 0 : assignment.graded}</td>
-                        <td>{assignment.maxPoints}</td>
-                        <td>avg</td>
-                        <td><Button component={Link} to={`/course/${this.state.courseId}/grade-assignments/${assignment.assignmentID}`}>Grade</Button></td>
-                    </tr>
-                ))}
-                </tbody>
-                </table>
-            </div>
-        )
-    }
+    //         ]
+    //     }
 }
-
-export default withStyles(styles)(CourseGradesInstructor)
