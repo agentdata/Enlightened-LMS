40ce4edd1a95ecf85c1db3c45cb2b442773d3fd8
JPD333 <jordanpdavis@hotmail.com>
2020-08-04 21:52:01 -0600

    Notification list updates when clearing notifications. No notifications message now showing. Matched "no notifications" message style to match.

--- React-Frontend/src/components/Notifications.js
+++ React-Frontend/src/components/Notifications.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -8,48 +8,6 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link'
 import http from "../api/http";
 import utilities from "../actions/utilities";
-
-const state = {
-
-    notifications: [
-        {
-            id: "123456",
-            title: "New comment for \"Assignment 4 Fork Help\"",
-            courseName: "CS 3100",
-            timestamp: "2020-07-20T11:51:00",
-            link: "link to assignment",
-            cleared: false
-        }
-    ]
-}
-
-const getNotifications = () => {
-
-    http.getUserNotifications()
-        .then( async (response) => {
-            const data = await response.json();
-            if (response.status === 200 && data["message"] === "success") {
-                state.notifications = data["newNotifications"];
-            }
-        })
-        .catch((e) => {
-            console.warn("There was an error retrieving notifications: ", e);
-        })
-}
-
-const clearNotification = (currentNotification) => {
-    http.clearUserNotification(currentNotification.id)
-        .then( async (response) => {
-            const data = await response.json();
-            if (response.status === 200 && data["message"] === "success") {
-                state.notifications = state.notifications.filter((value, index, arr)=>{return value.id !== currentNotification.id;})
-                // need to figure out how to refresh the list without having to close the notifcations and reopen
-            }
-        })
-        .catch((e) => {
-            console.warn("There was an error retrieving notifications: ", e);
-        })
-}
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -76,43 +34,77 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function AlignItemsList() {
-  const classes = useStyles();
-  getNotifications();
+    const classes = useStyles();
 
-  return (
-    <List className={classes.root}>
-        {state.notifications ? (
-            state.notifications.map(currentNotification => (
-                <div key={ currentNotification.id }>
-                <ListItem alignItems="flex-start">
-                    <Link href={currentNotification.link}>
-                        <ListItemText className={classes.title}
-                            primary={currentNotification.title}
-                            secondary={
-                                <React.Fragment>
-                                    <Typography
-                                        component="span"
-                                        variant="body2"
-                                        className={classes.inline}
-                                        color="textPrimary"
-                                        >
-                                            {currentNotification.courseName}
-                                        </Typography>
-                                        <br />
-                                        { utilities.formatDateTime(currentNotification.timestamp) }
-                                    </React.Fragment>
-                                }
-                            />
-                    </Link>
-                    <button type="button" className={classes.clearButton} onClick={() => clearNotification(currentNotification, state)} >
-                        <span className={classes.clearIcon}>&times;</span>
-                    </button>
-                </ListItem>
-                <Divider />
+    const [notifications, setNotifications] = React.useState([]);
+
+    useEffect(() => {
+        var fetchedNotifications = []
+        http.getUserNotifications()
+            .then( async (response) => {
+                const body = await response.json();
+                if (response.status === 200 && body["message"] === "success") {
+                    fetchedNotifications = body["newNotifications"];
+                }
+                setNotifications(fetchedNotifications)
+            })
+            .catch((e) => {
+                console.warn("There was an error retrieving notifications: ", e);
+            })
+
+    });
+
+    const clearNotification = (currentNotification) => {
+        http.clearUserNotification(currentNotification.id)
+            .then( async (response) => {
+                const data = await response.json();
+                if (response.status === 200 && data["message"] === "success") {
+                    setNotifications(notifications.filter((value, index, arr)=>{return value.id !== currentNotification.id;}))
+                }
+            })
+            .catch((e) => {
+                console.warn("There was an error retrieving notifications: ", e);
+            })
+    }
+
+    return (
+        <List className={classes.root}>
+            {notifications.length > 0 ? (
+                notifications.map(currentNotification => (
+                    <div key={ currentNotification.id }>
+                    <ListItem alignItems="flex-start">
+                        <Link href={currentNotification.link}>
+                            <ListItemText className={classes.title}
+                                primary={currentNotification.title}
+                                secondary={
+                                    <React.Fragment>
+                                        <Typography
+                                            component="span"
+                                            variant="body2"
+                                            className={classes.inline}
+                                            color="textPrimary"
+                                            >
+                                                {currentNotification.courseName}
+                                            </Typography>
+                                            <br />
+                                            { utilities.formatDateTime(currentNotification.timestamp) }
+                                        </React.Fragment>
+                                    }
+                                />
+                        </Link>
+                        <button type="button" className={classes.clearButton} onClick={() => clearNotification(currentNotification)} >
+                            <span className={classes.clearIcon}>&times;</span>
+                        </button>
+                    </ListItem>
+                    <Divider />
+                    </div>
+                ))
+            ) : <div>
+                    <ListItem alignItems="flex-start">
+                        <ListItemText className={classes.title} primary="No Notifications"/>
+                    </ListItem>
                 </div>
-            ))
-        ) : <div>No notifications</div>
-        }
-    </List>
-  );
+            }
+        </List>
+    );
 }
